8771ca470d25ee2a9a75d137708d2538e27407c2
Kate Foust <kate.e.foust@gmail.com>
2020-01-22 19:36:49 -0800

    added button to student page

--- assets/javascript/giphyapi.js
+++ assets/javascript/giphyapi.js
@@ -3,6 +3,13 @@ $(document).ready(function(){
     //array of celebration gifs
     var congratsGif= ["Reading Rainbow", "Arthur", "Harley Quinn"]
 
+  
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + reading + "&api_key=GbRihUv0Hb08GrTIBghIXh1vFSkQWBxt&limit=10";
+        console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "Get"
+        })
 
 
 });
